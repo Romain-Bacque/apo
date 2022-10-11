22844fe30594b4ecd172bc0d33acebd2044c5141
Jordan <lombard.jordan1995@gmail.com>
2022-10-11 16:32:32 +0200

    style: map markers

--- src/components/Map/MypositionIcon.js
+++ src/components/Map/MypositionIcon.js
@@ -1,7 +1,7 @@
 import L from "leaflet";
 
 export default L.icon({
-  iconSize: [40, 45],
+  iconSize: [40, 60],
   iconAnchor: [14, 41],
   popupAnchor: [2, -40],
   iconUrl: require ("./myposition.png"),
